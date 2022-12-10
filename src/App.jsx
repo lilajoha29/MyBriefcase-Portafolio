88444bc5130fd67a08db1770d83136db2f585f8d
Lily Johana <lilajoha2907@gmail.com>
2022-12-09 22:42:40 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
