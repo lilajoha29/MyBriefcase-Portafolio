a48332a467c4f49d827b36c89504b1b5d75efde2
lilajoha29 <lilajoha2907@gmail.com>
2024-03-25 12:24:40 -0500

    actualizacion hv

--- src/componentes/Hv/MyHv.jsx
+++ src/componentes/Hv/MyHv.jsx
@@ -8,7 +8,7 @@ export const MyHv = () => {
             <div className='flex flex-col py-3'>
                 <h2 className="font-DynaPuff text-rosado text-2xl flex flex-row m-1 pb-3 lg:text-3xl">Perfil <GiCowled className='m-1 animate-pulse hover:animate-ping' /></h2>
             <div id="perfil" className=" flex flex-col items-center ">
-                <p className="font-DynaPuff text-blanco  flex flex-col text-left sm:px-10 lg:text-lg" >Soy desarrolladora web FronEnd junior con conocimientos en HTML, CSS, ReactJS, JavaScript y metodología Scrum con conocimientos en Mercadeo y Publicidad, manejo de target, Insight e investigación de mercado, lo cual permite desarrollar mejor un UX. Tengo habilidades en liderazgo, comunicación asertiva, solución de conflictos y trabajo en equipo. Participe de forma voluntaria con la Red Nacional de Jóvenes de Ambiente a favor incentivar el cuidado ambiental.</p>
+                <p className="font-DynaPuff text-blanco  flex flex-col text-left sm:px-10 lg:text-lg" >Soy Lily Castillo desarrolladora web FrontEnd apasionada por el diseño web con conocimientos en Mercadeo y Publicidad, manejo de target, Insight e investigación de mercado, lo cual permite desarrollar mejor diseño UX y tener facilidades para realizar diseños de interfaz responsive. Tengo habilidades en liderazgo, comunicación asertiva, solución de conflictos y trabajo en equipo. Participe de forma voluntaria con la Red Nacional de Jóvenes de Ambiente a favor incentivar el cuidado ambiental.</p>
             </div>
         </div>
         {/* <div id="perfil" className=" flex flex-col items-center  ">
